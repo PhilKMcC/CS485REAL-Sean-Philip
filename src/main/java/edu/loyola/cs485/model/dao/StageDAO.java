f23a22317bcb3046f68922cca0cc754ec695a2a2
PhilKMcC <pkmccaffrey@loyola.edu>
2025-12-03 23:06:12 -0500

    outlined DAO methods

--- src/main/java/edu/loyola/cs485/model/dao/StageDAO.java
+++ src/main/java/edu/loyola/cs485/model/dao/StageDAO.java
@@ -1,7 +1,34 @@
 package edu.loyola.cs485.model.dao;
 
-public class StageDAO extends AbstractDAO{
+import edu.loyola.cs485.model.entity.Stage;
 
+import java.sql.SQLException;
+import java.util.List;
 
+public class StageDAO extends AbstractDAO<Stage>{
 
+    @Override
+    public void create(Stage entity) throws SQLException {
+
+    }
+
+    @Override
+    public Stage read(int ID) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(Stage entity) throws SQLException {
+
+    }
+
+    @Override
+    public void delete(int ID) throws SQLException {
+
+    }
+
+    @Override
+    public List<Stage> list() throws SQLException {
+        return List.of();
+    }
 }
