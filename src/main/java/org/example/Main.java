964f74f2d77534951c09d52aff3130bbfa7c920a
seantriplett <srtriplett@loyola.edu>
2025-12-03 22:21:40 -0500

    Added a bit of formatting to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import edu.loyola.cs485.model.dao.StageDAO;
+import edu.loyola.cs485.model.entity.Stage;
+import edu.loyola.cs485.view.MainFrame;
+
+import java.sql.*;
+import java.util.List;
 public class Main {
+    static String ConURL = "jdbc:mysql://localhost";
+    static String Port = "3306";
+    //static String Database = "";
+    //static String Username = "";
+    //static String Password = "";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
     }
 }
