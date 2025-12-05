1b00a49ca6096d6841c451f71af661c32a173edf
PhilKMcC <pkmccaffrey@loyola.edu>
2025-12-04 23:21:24 -0500

    updated testing

--- src/test/java/edu/loyola/cs485/model/dao/StageDAOTest.java
+++ src/test/java/edu/loyola/cs485/model/dao/StageDAOTest.java
@@ -2,12 +2,31 @@ package edu.loyola.cs485.model.dao;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-import edu.loyola.cs485.model.entity.Stage;
 import edu.loyola.cs485.model.dao.StageDAO;
+import edu.loyola.cs485.model.entity.Stage;
 
+import java.sql.SQLException;
 
 
 public class StageDAOTest {
+
+    @Test
+    public void TestCreateStage() throws Exception{
+        StageDAO dao = new StageDAO();
+        dao.setTestDatabase();
+        Stage stage = new Stage();
+
+        stage.setName("mansion");
+        stage.setSize("big");
+        stage.setLayout("standard");
+
+        dao.create(stage);
+
+        assertAll(
+                () -> assertNotNull(stage.getID())
+        );
+    }
+
 
     @Test
     public void testReadClient() throws Exception{
