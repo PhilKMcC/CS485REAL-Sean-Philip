b33760d58630d4d20f33114adaad5c4f5e605138
seantriplett <srtriplett@loyola.edu>
2025-12-04 20:39:58 -0500

    Added credentials

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,9 @@ import java.util.List;
 public class Main {
     static String ConURL = "jdbc:mysql://localhost";
     static String Port = "3306";
-    //static String Database = "";
-    //static String Username = "";
-    //static String Password = "";
+    static String Database = "game_db";
+    static String Username = "cs485";
+    static String Password = "groupproject14";
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
