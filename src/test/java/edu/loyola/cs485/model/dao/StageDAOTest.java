0eb00b31064bc833f671acd3c2eefd16ae6103f0
seantriplett <srtriplett@loyola.edu>
2025-12-04 23:19:18 -0500

    Added read

--- src/test/java/edu/loyola/cs485/model/dao/StageDAOTest.java
+++ src/test/java/edu/loyola/cs485/model/dao/StageDAOTest.java
@@ -2,7 +2,35 @@ package edu.loyola.cs485.model.dao;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import edu.loyola.cs485.model.entity.Stage;
+import edu.loyola.cs485.model.dao.StageDAO;
+
 
 
 public class StageDAOTest {
+
+    @Test
+    public void testReadClient() throws Exception{
+        StageDAO dao = new StageDAO();
+        dao.setTestDatabase();
+
+        Stage stage = new Stage();
+        stage.setName("Testing Stage");
+        stage.setSize("medium");
+        stage.setLayout("Test Layout");
+
+        dao.create(stage);
+        Stage found = dao.read(stage.getID()); //Function under test
+
+        //clean up
+        dao.delete( stage.getID() );
+
+        assertAll(
+                () -> assertEquals(found.getID(), stage.getID()),
+                () -> assertEquals(found.getName(), stage.getName()),
+                () -> assertEquals(found.getSize(), stage.getSize()),
+                () -> assertEquals(found.getLayout(), stage.getLayout())
+        );
+
+    }
 }
